1cc927e1f1c1475cde9ce223ba55d850b6fe9490
Jay Panchal <jkpanchal3491@gmail.com>
2026-02-19 10:01:18 +0530

    Add some login changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { logout } from "../features/auth/authSlice";
 
 export default function Navbar() {
-  const { token } = useSelector((state) => state.auth);
+  const { token, user } = useSelector((state) => state.auth);
   const { items } = useSelector((state) => state.cart);
   const cartCount = items.length;
   const dispatch = useDispatch();
@@ -20,9 +20,15 @@ export default function Navbar() {
           {!token ? (
             <Link to="/login" className="nav-link btn-login">Login</Link>
           ) : (
-            <button className="btn-logout" onClick={() => dispatch(logout())}>
-              Logout
-            </button>
+            <div className="user-dropdown">
+              <span className="user-icon-trigger"><i className="bi bi-person"></i></span>
+              <div className="dropdown-menu">
+                <span className="dropdown-username">Hello, {user?.username}</span>
+                <button className="dropdown-logout" onClick={() => dispatch(logout())}>
+                  Logout
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </div>
